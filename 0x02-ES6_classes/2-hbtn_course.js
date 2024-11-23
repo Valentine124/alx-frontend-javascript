6309c1c1e991f9339c6a949af234955ca9d8fa83
Valentine124 <kaluvalentine60@gmail.com>
2024-11-23 03:41:25 +0100

    Task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -18,14 +18,26 @@ export default class HolbertonCourse {
   }
 
   get name() {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
     return this._name;
   }
 
   get length() {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
     return this._length;
   }
 
   get students() {
+    if (typeof students !== 'object') {
+      throw new TypeError('Length must be an object');
+    }
+
     return this._students;
   }
 
